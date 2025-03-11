f93ad19d33c70f8217b89cfe8a35d7227a8a5f89
Mar-cialobo <marcialobo-2@hotmail.com>
2025-03-11 20:39:54 -0300

    Segundo Coomit

---
+++ frontend/src/pages/Auth/Login.js
@@ -0,0 +1,10 @@
+// frontend\src\pages\Auth\Login.js
+import "./Auth.css";
+
+const Login = () => {
+    return (
+        <div>Login</div>
+    )
+}
+
+export default Login
---
+++ frontend/src/pages/Home/Home.js
@@ -0,0 +1,10 @@
+// frontend\src\pages\Home\Home.js
+import "./Home.css";
+
+const Home = () => {
+    return (
+        <div>Home</div>
+    )
+}
+
+export default Home
