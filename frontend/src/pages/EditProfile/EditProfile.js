eb7e228661fab05521afe5323f78417cee296976
Mar-cialobo <marcialobo-2@hotmail.com>
2025-03-17 23:07:09 -0300

    Co ntinuação do Projeto

--- frontend/src/pages/EditProfile/EditProfile.js
+++ frontend/src/pages/EditProfile/EditProfile.js
@@ -3,6 +3,10 @@ import "./EditProfile.css";
 import React from "react";
 
 const EditProfile = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div id="edit-profile">
       <h2>Edite seus dados</h2>
@@ -10,6 +14,23 @@ const EditProfile = () => {
         Adicione uma imagem de perfil e conte mais sobre voce...
       </p>
       {/* preview da imagem */}
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Nome" />
+        <input type="email" placeholder="E-mail" disabled />
+        <label>
+          <span>Imagem do Perfil:</span>
+          <input type="file" />
+        </label>
+        <label>
+          <span>Bio:</span>
+          <input type="text" placeholder="Descrição do perfil" />
+        </label>
+        <label>
+          <span>Quer alterar sua senha?</span>
+          <input type="password" placeholder="Digite sua nova senha" />
+        </label>
+        <input type="submit" value="Atualizar" />
+      </form>
     </div>
   );
 };
