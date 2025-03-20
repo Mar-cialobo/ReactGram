1e616170b31c1a8658fdcb4220d4a6a0a61612d0
Mar-cialobo <marcialobo-2@hotmail.com>
2025-03-19 21:53:32 -0300

    Atualização

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,6 +15,7 @@ import Register from "./pages/Auth/Register";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import EditProfile from "./pages/EditProfile/EditProfile";
+import Profile from "./pages/Profile/Profile";
 
 function App() {
   const { auth, loading } = useAuth();
@@ -38,6 +39,10 @@ function App() {
             <Route
               path="/profile"
               element={auth ? <EditProfile /> : <Navigate to="/login" />}
+            />
+            <Route
+              path="/users/:id"
+              element={auth ? <Profile /> : <Navigate to="/login" />}
             />
             <Route
               path="/login"
