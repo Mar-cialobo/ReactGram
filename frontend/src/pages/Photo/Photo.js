4ab8a80b280dc17a342663cf81d80039a38da5a3
Mar-cialobo <marcialobo-2@hotmail.com>
2025-04-04 21:41:01 -0300

    commit atualização

--- frontend/src/pages/Photo/Photo.js
+++ frontend/src/pages/Photo/Photo.js
@@ -1,3 +1,4 @@
+// frontend\src\pages\Photo\Photo.js
 import './Photo.css';
 
 import { uploads } from '../../utils/config';
@@ -11,14 +12,18 @@ import PhotoItem from '../../components/PhotoItem';
 import { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import { useResetComponentMessage } from '../../hooks/useResetComponentMessage';
 
 // Redux
-import { getPhoto } from '../../slices/photoSlice';
+import { getPhoto, like } from '../../slices/photoSlice';
+import LikeContainer from '../../components/LikeContainer';
 
 const Photo = () => {
   const { id } = useParams();
 
   const dispatch = useDispatch();
+
+  const resetMessage = useResetComponentMessage(dispatch);
 
   const { user } = useSelector((state) => state.auth);
   const { photo, loading, error, message } = useSelector(
@@ -32,7 +37,11 @@ const Photo = () => {
     dispatch(getPhoto(id));
   }, [dispatch, id]);
 
-  // like e comentário
+  const handleLike = () => {
+    dispatch(like(photo._id));
+
+    resetMessage();
+  };
 
   if (loading) {
     return <p>Carregando...</p>;
@@ -41,6 +50,11 @@ const Photo = () => {
   return (
     <div id='photo'>
       <PhotoItem photo={photo} />
+      <LikeContainer photo={photo} user={user} handleLike={handleLike} />
+      <div className='message-container'>
+        {error && <Message msg={error} type='error' />}
+        {message && <Message msg={message} type='success' />}
+      </div>
     </div>
   );
 };
