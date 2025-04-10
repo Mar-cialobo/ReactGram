02aa911040dc8b7dba09b07841ff2b7024fd9160
Mar-cialobo <marcialobo-2@hotmail.com>
2025-04-09 22:03:00 -0300

    commit

--- frontend/src/services/photoService.js
+++ frontend/src/services/photoService.js
@@ -91,6 +91,36 @@ const like = async (id, token) => {
   }
 };
 
+// Add comment to a photo
+const comment = async (data, id, token) => {
+  const config = requestConfig('PUT', data, token);
+
+  try {
+    const res = await fetch(api + '/photos/comment/' + id, config)
+      .then((res) => res.json())
+      .catch((err) => err);
+
+    return res;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// Get all photos
+const getPhotos = async (token) => {
+  const config = requestConfig('GET', null, token);
+
+  try {
+    const res = await fetch(api + '/photos', config)
+      .then((res) => res.json())
+      .catch((err) => err);
+
+    return res;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 const photoService = {
   publishPhoto,
   getUserPhotos,
@@ -98,6 +128,8 @@ const photoService = {
   updatePhoto,
   getPhoto,
   like,
+  comment,
+  getPhotos,
 };
 
 export default photoService;
